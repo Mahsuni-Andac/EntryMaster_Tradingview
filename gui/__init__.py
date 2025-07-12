45049e0eb45de1bbc572a9824accd2626832ffcf
Mahsuni-Andac <email.mahsuni@gmail.com>
2025-07-12 10:05:17 +0200

    Add neon status panel with tooltips

--- gui/__init__.py
+++ gui/__init__.py
@@ -4,9 +4,11 @@ from .trading_gui_core import TradingGUI
 from .trading_gui_logic import TradingGUILogicMixin
 
 from .api_credential_frame import APICredentialFrame
+from .neon_status_panel import NeonStatusPanel
 
 __all__ = [
     "TradingGUI",
     "TradingGUILogicMixin",
     "APICredentialFrame",
+    "NeonStatusPanel",
 ]
